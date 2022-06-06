ba98dcc5056e0f30a6358deabe0cf559901f3a20
Sebastian Falana <s.falana@azagroup.eu>
2022-06-06 14:38:42 +0200

    A BIG change for Homework_DataPickers

--- src/test/java/homework_SeleniumBasic/Homework_DataPickers.java
+++ src/test/java/homework_SeleniumBasic/Homework_DataPickers.java
@@ -10,82 +10,88 @@ import java.util.List;
 
 public class Homework_DataPickers extends TestBase {
 
-    String pickUserDate = "08/31/2022";
+    String pickUserDate = "08/25/2022";
+    int pickDayByUser = 25;
     int pickMonthByUser = 7;
     int pickYearByUser = 2022;
+
     @Test
-    public void shouldPickDate(){
+    public void shouldPickDate() {
         driver.get("https://seleniumui.moderntester.pl/datepicker.php");
         driver.findElement(By.cssSelector("#datepicker")).click();
 
-        if (getCurrentYear() == pickYearByUser){
+
+        if (getCurrentYear() == pickYearByUser) {
             pickCorrectMonth();
-        }else if(getCurrentYear() > pickYearByUser){
-            while (getCurrentYear() != pickYearByUser){
+            pickCorrectDay();
+        } else if (getCurrentYear() > pickYearByUser) {
+            while (getCurrentYear() != pickYearByUser) {
                 getPrevArrowHandler().click();
             }
-           pickCorrectMonth();
-        }else {
-            while (getCurrentYear() != pickYearByUser){
+            pickCorrectMonth();
+            pickCorrectDay();
+        } else {
+            while (getCurrentYear() != pickYearByUser) {
                 getNextArrowHandler().click();
             }
             pickCorrectMonth();
+            pickCorrectDay();
         }
 
         System.out.println(getDataPicker());
-        Assert.assertEquals(getDataPicker(),pickUserDate);
+        Assert.assertEquals(getDataPicker(), pickUserDate);
     }
 
-
-    public WebElement getNextArrowHandler(){
+    public WebElement getNextArrowHandler() {
         return driver.findElement(By.cssSelector("[class='ui-icon ui-icon-circle-triangle-e']"));
     }
-    public WebElement getPrevArrowHandler(){
+
+    public WebElement getPrevArrowHandler() {
         return driver.findElement(By.cssSelector("[class='ui-icon ui-icon-circle-triangle-w']"));
     }
 
-    public int getCurrentYear(){
-        return Integer.parseInt(getFifthRowForthCell().getAttribute("data-year"));
+    public WebElement getDatePickerYear() {
+        return driver.findElement(By.cssSelector(".ui-datepicker-year"));
     }
 
-    public int getCurrentMonth(){
-        return Integer.parseInt(getFifthRowForthCell().getAttribute("data-month"));
+    public List<WebElement> getDateTableElements() {
+        return driver.findElements(By.cssSelector("[data-handler='selectDay']"));
     }
 
-    public int getCurrentDay(){
-        return Integer.parseInt(getFifthRowForthCell().getAttribute("data-month"));
+    public int getCurrentYear() {
+        return Integer.parseInt(getDatePickerYear().getText());
     }
 
-    public WebElement getFifthRowForthCell(){
-        List<WebElement> rows = driver.findElements(By.cssSelector("tbody tr"));
-        WebElement fifthRow = rows.get(4);
-
-        List<WebElement> rowCells = fifthRow.findElements(By.cssSelector("td"));
-        return rowCells.get(3);
+    public int getMonth() {
+        return Integer.parseInt(getDateTableElements().get(7).getAttribute("data-month"));
     }
 
-    public String getDataPicker(){
+    public List<WebElement> getDaysOfCurrentMonthElements() {
+        return driver.findElements(By.cssSelector("a[class='ui-state-default']"));
+    }
+
+    public String getDataPicker() {
         return driver.findElement(By.cssSelector("#datepicker")).getAttribute("value");
     }
 
-    public void pickCorrectMonth(){
-        if (getCurrentMonth() == pickMonthByUser){
-            getFifthRowForthCell().click();
-        }else if(getCurrentMonth() > pickMonthByUser){
-            while (getCurrentMonth() != pickMonthByUser){
+    public void pickCorrectMonth() {
+        if (getMonth() > pickMonthByUser) {
+            while (getMonth() != pickMonthByUser) {
                 getPrevArrowHandler().click();
             }
-            getFifthRowForthCell().click();
-        }else {
-            while (getCurrentMonth() != pickMonthByUser){
+        } else {
+            while (getMonth() != pickMonthByUser) {
                 getNextArrowHandler().click();
             }
-            getFifthRowForthCell().click();
         }
     }
 
-    public void pickCorrectDay(){
-
+    public void pickCorrectDay() {
+        for (WebElement pickedDay : getDaysOfCurrentMonthElements()) {
+            if (Integer.parseInt(pickedDay.getText()) == pickDayByUser) {
+                pickedDay.click();
+            }
+        }
     }
 
 }
